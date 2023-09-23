1f45cfee9e8a6d15f96981278579eb3301a54cd4
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-09-24 02:28:47 +0600

    add constructor

--- classObject.cpp
+++ classObject.cpp
@@ -16,22 +16,34 @@ public:
         std::cout << "My Department is:" << Department << std::endl;
         std::cout << "My Age is:" << Age << std::endl;
     }
+    // add constructor
+    Student(string name, string department, int age)
+    {
+        Name = name;
+        Department = department;
+        Age = age;
+    }
 };
 
 int main()
 {
 
-    Student student1;
-    student1.Name = "Md Sujan";
-    student1.Department = "CSE";
-    student1.Age = 23;
-    student1.introduceYourself();
+    // Student student1;
+    // student1.Name = "Md Sujan";
+    // student1.Department = "CSE";
+    // student1.Age = 23;
+    // student1.introduceYourself();
 
-    Student student2;
-    student2.Name = "Sheme";
-    student2.Department = "CSE";
-    student2.Age = 23;
+    // Student student2;
+    // student2.Name = "Sheme";
+    // student2.Department = "CSE";
+    // student2.Age = 23;
+    // student2.introduceYourself();
+    Student student1 = Student("Sheme", "CSE", 23);
+    student1.introduceYourself();
+    Student student2 = Student("Sujan", "CSE", 23);
     student2.introduceYourself();
 
+    
     return 0;
 }
