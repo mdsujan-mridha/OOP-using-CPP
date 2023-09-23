72136db43919812b40cb43ed4053d822634df9c1
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-09-24 02:16:47 +0600

    You can work with this method but there is problem.Problem is when i want push more object then its need to write more code that's why i ignore this methods

--- classObject.cpp
+++ classObject.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 using namespace std;
-using std :: string;
+using std ::string;
+
+class Student
+{
+public:
+    string Name;
+    string Department;
+    int Age;
+
+    void introduceYourself()
+    {
+
+        std::cout << "My name is:" << Name << std::endl;
+        std::cout << "My Department is:" << Department << std::endl;
+        std::cout << "My Age is:" << Age << std::endl;
+    }
+};
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    Student student1;
+    student1.Name = "Md Sujan";
+    student1.Department = "CSE";
+    student1.Age = 23;
+    student1.introduceYourself();
+
+    Student student2;
+    student2.Name = "Sheme";
+    student2.Department = "CSE";
+    student2.Age = 23;
+    student2.introduceYourself();
+
     return 0;
 }
